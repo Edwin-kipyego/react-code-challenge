cebde9f72cbd3cdf262e79b4cf6e5a1e8721abc5
Edwin kipyego <koriredwin95@gmail.com>
2025-04-06 21:46:24 +0300

    learning project

---
+++ phase1/EDWIN/index.js
@@ -0,0 +1,59 @@
+// let age = 25;
+const name ="Edwin";
+//age =30;
+//console.log(`Hello ${name}, you are ${age} years old.`);
+
+
+
+//let firstName = "Edwin";
+//let lastName = "Kipkorir";
+//let fullNmae = firstName + "" + lastName;
+//console.log(fullNmae);
+
+// strings with other data types
+//let age = 35;
+//let message = "Hello, my name is " + firstName + " and I am " + age + " years old.";
+//console.log(message);
+let firstName = "Edwin";
+let lastName = "Kipkorir";
+//let age = 30;
+//let greeting = `Hello, my name is ${firstName} ${lastName} and I am ${age} years old.`;
+//console.log(greeting); 
+
+
+let a = 5;
+let b = 10;
+let result = `The sum of ${a} and ${b} is ${a + b}.`;
+console.log(result);
+//let message = "hello";
+//let upperMessage = message.toUpperCase();
+//console.log(upperMessage);
+
+
+let message = "HELLO";
+let lowerMessage = message.toLowerCase();
+console.log(lowerMessage); 
+
+
+//let age = 16;
+//if (age >= 18) {
+   // console.log("You are an adult.");
+//}
+//else {
+   // console.log("You are a minor.");
+//}
+
+//let age = 16;
+
+//let age =25;
+//if (age < 13){
+   // console.log("you are a child");
+//}else if (age <20){
+    //console.log("you are a teenager");
+//}else{
+  //  console.log("you are an adult");
+//}
+//ternary operator
+let age = 20;
+let status = (age >= 18) ? "Adult." : "minor.";
+console.log(status);
